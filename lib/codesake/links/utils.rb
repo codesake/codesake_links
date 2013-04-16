423af395a5a24244489a89d3ac687a93ef560034
Paolo Perego <thesp0nge@gmail.com>
2013-04-16 17:13:13 +0200

    Improved messages

--- lib/codesake/links/utils.rb
+++ lib/codesake/links/utils.rb
@@ -4,15 +4,15 @@ module Codesake
     class Utils
 
       def self.print_str(url, logger, str, start, stop)
-        logger.ok "#{url}: #{str} (#{((stop-start) * 1000).round} msec)\n" if str == "Open"
-        logger.log "#{url}: #{str} (#{((stop-start) * 1000).round} msec)\n" unless str == "Open"
+        logger.ok "#{url}: #{str} (#{((stop-start) * 1000).round} msec)" if str == "Open"
+        logger.log "#{url}: #{str} (#{((stop-start) * 1000).round} msec)" unless str == "Open"
 
         return
       end
 
       def self.print_code(url, logger, code, start, stop)
-        logger.ok "#{url}: #{code} (#{((stop-start) * 1000).round} msec)\n" if code == 200
-        logger.log "#{url}: #{code} (#{((stop-start) * 1000).round} msec)\n" unless code == 200
+        logger.ok "#{url}: #{code} (#{((stop-start) * 1000).round} msec)" if code == 200
+        logger.log "#{url}: #{code} (#{((stop-start) * 1000).round} msec)" unless code == 200
 
         return
       end
